cba12a918f68a4bcdd11a5c164369ff644f357b6
invisibileperso <invisibileperso@posteo.com>
2025-01-03 13:39:25 +0100

    Add files via upload
    separato i testi dal file index

---
+++ entries.js
@@ -0,0 +1,140 @@
+const entries = {
+    "1": {
+        spotify: `<iframe style="border-radius:12px" 
+            src="https://open.spotify.com/embed/episode/1i3ENU6FmnIteMc0h5rlUb?utm_source=generator" 
+            width="100%" 
+            height="152" 
+            frameBorder="0" 
+            allowfullscreen="" 
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+            loading="lazy">
+        </iframe>`,
+        content: `
+        Primo gennaio 2025, nuovo anno, prima puntata
+
+        E' d'obbligo darvi due informazioni su quello che sarà Invisibile Perso
+        
+        Sarà Un percorso.
+        Sarà un viaggio di scoperta interiore per cambiare o accettare.
+        Un viaggio per Ricercare le emozioni, sensazioni espulse dalla quotidianità tanto più per un essere vivente di sesso maschile nel mezzo di un travaglio lavorativo come il sottoscritto.
+        
+        La difficoltà di accettarsi, di trovarsi nel mezzo della vita con un pesante senso di incompiuto e una costante frustrazione quotidiana nel sentirsi estraneo, arretrato rispetto all'attuale.
+        
+        Se guardo al lato felice mi chiedo se l'essere marito e papà possa bastare? 
+        Forse si, forse ad alcuni si, forse dovrebbe bastarmi. 
+        Marito amato e padre anche, spero, spero d'esempio in ogni caso; dovrebbe bastarmi?
+        
+        Che poi, mi chiedo anche se non sia l'ego che mi impedisce di accettarmi? 
+        Oppure la smaniosa voglia di capire le cose e di non accettare la pappa pronta, 
+        La voglia di non arrendersi all'evidenza dei fatti o semplicemente dovrei rendermi conto delle mie mancate capacità personali?
+        
+        Non lo so, spesso forse come in queste righe c'è molta confusione, c'è molta ignoranza, quella si perché non ho mai studiato il "noi" avendo sempre preferito, forse sbagliando, la parte materica. Sono Geometra, che errore, poi Geologo che amara gioia, e quindi terreno per definizione.
+        
+        Nei libri che ora ho per le mani si parla del presente, di stare nel presente, ma l'orizzonte, la prospettiva, la progettualità dove la mettiamo? 
+        Sono io che non capisco.
+        
+        Ma aspettate non voglio sicuramente fare il guru, anzi lungi da me accarezzare quella cosa che chiamano Crescita Personale.
+        Cercherò di mischiare l'attualità, le incazzature, la mia voglia perenne di rimettermi in viaggio, chissà magari con l'amata bici o magari a piedi o magari solamente con la penna.
+        In mezzo troverete anche sport, non potrei viver senza e tutte le mie svariate passioni: foto, disegno, ambiente, yoga. Ma non saranno questi il nocciolo della questione, sarranno b-roll per sostenere la vita.
+        Saranno puntate brevi, necessariamente brevi, senza fronzoli, scritte e registrate. Una puntata al giorno almeno per i giorni lavorativi. Niente effetti speciali o montaggi holliwoodiani. 
+        
+        Aspettate nuovamente: Non mi conoscete, ovvio, è la prima puntata, ma talvolta riesco bene nell'ironia e nel cinismo. Saranno sicuramente ingredienti di questo impasto di vita.
+        
+        Invisibile perso è una messa in discussione, una messa alla prova, un percorso spero partecipato per migliorare, se non la vita, almeno la scrittura e la produzione di questi audio.
+        Ei, non sono l'ultimo invasato di podcast, so bene come funziona. Sono su questa piattaforma in altre forme di vita dal lontano 2018. Sono ben cosciente del rischio di lanciare un granello di sabbia nell'oceano infestato di squali, ma semplicemente me ne fotto. 
+        
+        Ve lo dico già. Tutto questo ha una data di scadenza: 31/12/2025. Un anno di viaggio, un anno per aprire porte, per esplorare nuove strade o fare in modo che tutto ciò possa accadere.
+        
+        Avremo modo di conoscerci, ma non cercate l'infinito perso nei social o in altre piattaforme perchè non esiste e non esisterà. Non vi saranno newsletter o altre forme di comunicazione se non questo strumento. Un buon compromesso è la presenza qui su spotify e null'altro se non il sito dove troverete i testi  di queste puntate, forse qualche immagine e gli audio di questo cammino, spero comune.
+        
+        L'unico modo per comunicare sarà via mail.
+        
+        A domani.`
+    },
+    "2": {
+        spotify: `<iframe style="border-radius:12px" 
+            src="https://open.spotify.com/embed/episode/1uUGq5xW32sd26hco9kdZE?utm_source=generator" 
+            width="100%" 
+            height="152" 
+            frameBorder="0" 
+            allowfullscreen="" 
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+            loading="lazy">
+        </iframe>`,
+        content: `
+        Primo dell'anno.
+        Fisicamente energica, emotivamente serena, spiritualmente piena.
+        Questa in teoria sarebbe dovuta essere la mia prima giornata dell'anno 2025 che a dirla tutta non mi piace molto come numero. 
+        Non sono per niente numerologo e non credo nemmeno all'oroscopo o derivati, ma alcuni numeri mi danno più simpatia di altri. I dispari molto più simpatici dei pari perciò il 2025 dovrebbe starmi simpatico, ma ci sono troppi 2; vabbè sono le otto del mattino del giorno dopo, perdonatemi per queste digressioni.
+
+        Tornando alla mia giornata, l'aspetto fisico l'ho centrato, sono andato a correre e ho retto degnamente fino a sera, l'aspetto emotivo pure. E' stata una giornata serena, emotivamente stabile, piacevole, mentre il lato spirituale non è pervenuto. La spiritualità durante una mia giornata è relegata a non so bene quali momenti, penso non ce ne siano: quando medito? Non penso si possa parlare di spiritualità. 
+        Non sono credente nel senso general italico, non ho abbracciato altre religioni e a pensarci l'aspetto spirituale è veramente misero nel trascorrere delle giornate. Vabbè che inizio pesante...Ci tornerò su questo argomento, ma ora stappiamo un pò di bottiglie per questo nuovo anno!
+        Festeggiato? Brindato? Scoppiato fuochi? (Non ne ho mai capito il divertimento)
+        Avete contato i secondi con Liorni mentre l'altro povero mandava a quel paese il fonico che stava facendo correttamente il suo lavoro? 
+        Fermi tutti.
+        Era la notte di capodanno tra il 1994 e il 1995, si stava per chiudere un anno durante il quale segnammo l'ascesa del Berlusca, lanciammo le monetine sulla pelata craxiana, guardavamo con insulsa indifferenza una tragica e assurda guerra a due passi da noi e due miti ci lasciarono, uno per volontà sua, Kurt Cobain, l'altro per un tragico incidente: Ayrton Senna. 
+        Ma il fatto veramente importante stava per avvenire in un tunnel che collegava la strada che porta al centro del piccolo paese della bassa padovana, con l'interno del patronato dove era in corso la super festa di fine anno organizzata. Un giovane adolescente chiese per la quarta volta la mano alla sua amata. Lui un pò stronzetto, uno degli organizzatori di quel festone, dall'aspetto piacevole, un pò fuori dal coro, non l'adone, ma diciamo il tipo. Lei simpatica, scherzosa, molto carina, dalla chiacchiera facile e spedita, mora con occhi grandi che stregavano. Dicevamo, quarta volta, non perché le altre fossero andate buche, ma perché c'erano stati un pò di tira e molla. Si fu la risposta e da allora sono compagni di vita. Pazzesco, tornando ai numeri il 64% della vita sua e il 68% della vita sua di lei l'hanno trascorso insieme. Penso sia l'unica costante della sua vita di quel stronzetto dalle mille passioni, la colonna portante di tutto quello che gira attorno.
+        La vita del pianeta continuò anche nel 1995 e sembra continuare tuttora.
+
+        Sapete una una cosa: non ho mai amato tantissimo le feste dell'ultimo dell'anno perché non ho mai capito il vero senso del festeggiamento. Festeggiamo perchè si chiude un anno o perché se ne apre uno nuovo. Probabilmente entrambe le cose. 
+        Tanto più che si è obbligati a questi festeggiamenti di gruppo. Solitario come sono, molto spesso in quel frangente del botto con la bottiglia, servirebbe un pò di solitudine per concentrarsi veramente su quel che è stato e sul probabile sarà.
+        Tanto più in questo periodo dove il domani è incerto. Capitemi l'incertezza sta nei desideri di cambiamento, non sullo stato di fatto. Un vecchio saggio cresciuto negli anni 60-70 mi darebbe una bella pacca sulla spalla e mi direbbe: cosa ti lamenti! hai un lavoro fisso, una splendida famiglia, quella giovane ragazza dagli occhi grandi e neri che è cresciuta con te per il 68% della sua vita, una casa, pure un cane e una gatta! 
+        Porca zozza è vero, ma c'è quel ma, quei ma espressi ieri che saranno leciti o no?
+
+        Chiudo, mi dilungo, i saldi incalzano, il tempo passa e devo registrare l'audio.
+
+        A domani`
+    },
+    "3": {
+        spotify: `<iframe style="border-radius:12px" 
+            src="https://open.spotify.com/embed/episode/0Kf4ZPwGBnwxHkjBAXp9UY?utm_source=generator" 
+            width="100%" 
+            height="152" 
+            frameBorder="0" 
+            allowfullscreen="" 
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+            loading="lazy">
+        </iframe>`,
+        content: `
+        ### Istruzioni per l'uso 
+Terzo giorno, bè non male, sono su per giù ad un centesimo dall'obiettivo finale, ce la posso fare.
+Situazione odierna non ottimale, quindi abbiate pietà di quel che sentirete o leggerete.
+Ho procrastinato leggermente la scrittura di questa puntata vista la festività di queste giornate, ma come ogni santissima volta, ogni rimando aumenta l'entropia casereccia. Risultato che ora, ovviamente sono tutte sveglie, ovvio. 
+Rebbi mi ha usurpato la scrivania per fare una ricerca scolastica e fin qui...vabbè. Ma se poi nell'atto di recuperare il pc getto lo sguardo sul monitor e leggo: Osservato Romano non mi resta che soffocare un....eccccchecacccccchioooo eeee nooooo e puntare lo sguardo più interrogatorio che posso verso quel cespuglio di capelli neri che sta seduto sulla mia sedia.
+(eeee papi, ricerca di religione...)
+Regola 1: se devi fare una cosa, falla subito!!! 
+Dai questa sa proprio da fantasista, da numero 10 della crescita personale, 
+no comment 
+ma in fondo è vero no? 
+Tutte le volte che pensiamo di rimandare, tanto vado dopo a correre, dai vado in pausa pranzo via in bici, dai scrivo dopo. Tra quel pensiero e la realizzazione dello stesso si intrecceranno mille altri piccole cose che faranno saltare il primo vero obiettivo.
+Eccomi qui quindi in cucina, con il mac, con le cuffie per non sentire le canzoni rock della Rebbi mentre scrive sull'osservatore romano e via con la terza puntata.
+Caspita il titolo è Istruzioni per l'Uso e invece son qui a parlar di tutt'altro, ho procrastinato ancora nel giro di 2000 caratteri.
+Istruzioni per l'uso, dai un attimo di attenzione.
+Necessarie perchè questo non è mica un podcast come gli altri. Alzi la mano chi ascolta o chi riesce ad ascoltare podcast indipendenti. Dai su alzate le manine. mmm direi pochissimi, quasi non riesco a vedere mani alzate.
+Vi sarete accorti pure voi che i "migliori" le avete viste le virgolette fatte con le dita vero? bè ecco, i migliori oramai fanno tutti parte delle grosse case di produzione con la potenza di fuoco di una portaerei a propulsione atomica. Gli indipendenti sono rimasti come piccole barchette a remi in viaggio tra #pointnemo e la terra del fuoco.
+Io mi sono appena dato la spinta con il piede dallo scoglio, inzuppandomi la scarpa e maledicendo l'acqua gelida. Ho con me due remi di diversa lunghezza e un lenzuolo da usare come vela. Dalla mia parte ho coraggio e la sicurezza che sarà comunque un'avventura che varrà la pena vivere!
+E quindi queste istruzioni?
+Ok vi spiego come funziona il gioco.
+Lo farò 5 giorni su 7 dal martedì al sabato.
+Sarà una serie di diario postumo con 24 ore di differita.
+Le puntate verranno pubblicate alla mattina, oggi già fin troppo tardi.
+C'è un sito di riferimento ospitato su github (in descrizione troverete il link)
+C'è una mail per comunicare, pure essa incredibilmente in descrizione
+Nulla più di questo, nessun canale social, nessuna newsletter o modalità di iscrizione.
+Non vi chiedo alcun supporto, non ve lo chiederò
+Questo è un mio percorso, un viaggio a parole per cercare le emozioni quotidiane durante l'avventura di un cambiamento. 
+Lo potrete fare quotidianamente o quando ne avrete voglia, ma sappiate che tutte le puntate saranno intrecciate tra loro, vi saranno trame che continueranno lungo la vita, non potrà che esser così no?
+Quindi dai salite a bordo e se vi va scrivetemi!
+Ma ieri?
+uuuu ieri
+Lo sapete no che a fine settimana iniziano i saldi, ma magicamente i saldi via wapp o telegram sono già iniziati e se possiedi quell'incredibile tessera, a dire il vero ormai se ne sbattono anche di quello potrai usufruire di sconti anticipati, solo per te, solo per il tuo portafogli, solo per i tuoi gusti!
+E allora come non approfittarne?
+5 ore in un mega negozio d'abbigliamento
+Prova, riprova, perchè cavolo mi son messo la camicia!
+Cambia la taglia di qua, trova il modello di la.
+Fai provare, cerca il camerino
+Questo ti piace? NO
+Questo mi piace! NOOO quello no
+Stremati`
+    }
+}; 
