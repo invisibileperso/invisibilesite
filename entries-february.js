40c0279320be4f703cb801c69c835d81bcdeda53
invisibileperso <invisibileperso@posteo.com>
2025-02-01 12:25:57 +0100

    Update entries-february.js

--- entries-february.js
+++ entries-february.js
@@ -1,7 +1,7 @@
 const entriesFebruary = {
     "24": {
         spotify: `<iframe style="border-radius:12px" 
-            src="https://open.spotify.com/embed/track/YOUR_TRACK_ID?utm_source=generator" 
+            src="ttps://open.spotify.com/embed/episode/4eJm0CUVi4qy5jzOZd9npa?utm_source=generator" 
             width="100%" 
             height="152" 
             frameBorder="0" 
@@ -9,7 +9,52 @@ const entriesFebruary = {
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
             loading="lazy">
         </iframe>`,
-        content: `Primo giorno di febbraio. Il tuo contenuto qui...`
+        content: `
+        **Primo febbraio duemilaventicinque**  
+**Puntata #24**
+
+Peccato per il numero quasi tondo.  
+Insomma, chiuso il primo mese, gran bel risultato. In questa puntata pensavo di tirare un po' di somme e dissertare su qualche considerazione riferita a questo esperimento tra scrittura e podcast terapeutico.
+
+Benvenuti nell'Invisibile, dove le parole diventano ponti tra ciò che siamo e ciò che potremmo essere. Un podcast di riflessioni non filtrate, alla ricerca di quei luoghi dell’anima che il tempo non può cancellare.  
+Aiutami a far conoscere questo progetto indipendente condividendolo con un tuo contatto.  
+Forza, prendi il telefono, clicca sui tre puntini, condividi l'episodio e… grazie!
+
+Lo ammetto, ho avuto qualche giornata difficile, soprattutto in questi ultimi giorni sotto l'impero del raffreddore, ma sono comunque orgoglioso. Mi sono messo davanti al monitor e in qualche modo sono riuscito a scrivere qualcosa di decente, spero. Ovvio che non essendo io Giorgio Bocca, può capitare che non tutte le puntate escano con il buco, ma sopportatemi, il disegno, la strategia comunicativa... ho ho senti qua... si vede nel lungo periodo.
+
+Se non lo sapete, le statistiche sui podcast sono ammantate da un'aura di mistero abbastanza fitta. Sono veritiere? Affidabili? Vale la pena sbatterci il naso? Eh, bisognerebbe chiederlo al sig. Spotify, ma insomma, visto che i numeri disponibili sono solo i loro, almeno per me che non pubblico attraverso altri distributori, ci fidiamo e li prendiamo per buoni.  
+Se tra di voi ci fosse qualche podcaster rispondo subito alla domanda: perché solo Spotify? Banalmente per una questione di tempo nel creare nuovi canali sugli altri player e gestire quel pochissimo traffico che genererebbe la pubblicazione su Amazon, Apple Podcast, ecc.  
+Insomma, quanti ascolti ho fatto? Alla fine di questo mese siamo prossimi ai 600 ascolti totali sulle 23 puntate pubblicate. Se fate una semplice divisione scoprirete il numero esiguo di ascolti per puntata.
+
+La cosa positiva però è il trend relativo agli ascolti che è in continua crescita, così come gli iscritti al canale che attualmente sono 35, mentre sono circa una sessantina le persone che ascoltano gli episodi.  
+Ve lo dicevo che siamo 2 gatti, per quello vi rompo le scatole con la condivisione.  
+La strada che ho deciso di intraprendere non è certo semplice per una ricerca di consenso, ma teniamo botta e proviamo a seguire ancora un po' questa strada.
+
+Ma al di là di tutto ciò, a me cosa me ne importa.  
+Beh, sapete, una volta che si inizia a giocare, anche se all'inizio lo fai per puro spirito di guarigione, e si inizia a vedere qualche risultato, eh come dire, l'occhio alla fine cade sulle statistiche. Per questo un po' di giorni fa ho eliminato l'app dallo smartphone, ma i maledetti algoritmi giocano proprio sulla nostra incapacità di stare concentrati sul lungo periodo e ci offrono costantemente dolcetti istantanei per farci cadere in trappola.  
+E se facessi un po' di campagna social, che male ci sarebbe? Se iniziassi a spammarlo in giro pesantemente, che male ci sarebbe?  
+Oltre al tempo necessario, il male prima o poi arriva sotto forma di insoddisfazione, sotto forma di ricerca continua del consenso che sicuramente distoglie dallo scrivere, dallo scopo principale.
+
+Tocchiamo un altro punto.  
+Il sito web.  
+So per certo che quasi nessuno lo abbia visitato, poco male.  
+Avevo in mente di farlo progredire in maniera più organica, ma anche lì la questione ricade sull'orologio che inesorabilmente scorre. Per fare cose interessanti serve una marea di tempo, e pensate, non ne ho così tanto. Ma a questo proposito giusto ieri mi è arrivato un messaggio da parte di un famoso guru:
+
+Per questo rimango minimale con il sito avente esclusiva funzione archivistica.  
+Ma su questo punto ci sarebbe una facile evoluzione.  
+Per scrivere e gestire appunti sto utilizzando OBSIDIAN, ne ho già parlato, è definito un second brain, vabbè, questo Obsidian offre la possibilità di trasformare l'ambiente di lavoro in un sito web in maniera molto semplice con la possibilità anche di pubblicare il grafo dei collegamenti tra concetti e parole chiave: una cosa fighissima che spero prima o poi di farvi vedere. Ovviamente tutto ciò ha un prezzo che per ora non posso permettermi e come mi ero promesso fin dall'inizio Invisibile Perso non chiederà aiuto ai lettori, quindi per ora ciccia!
+
+Come proseguirò? Facile, così come ho iniziato: sedendomi davanti al pc, al monitor nero della nota giornaliera, scrivendo con più o meno ironia quello che mi attraversa nella vita quotidiana e dando voce a quelle parole.  
+Ho pensato che forse sarebbe carino far intervenire qualche esperto, piano, non intendo quelli dei metodi infallibili, ma qualche esperto di meditazione, di burnout, di psicologia, fare delle puntate "special" con l'ospite, che ne dite?  
+Nella descrizione dell'episodio troverete un sondaggio su questo, ditemi la vostra. Nel caso vincesse il sì, vedrò di attivarmi.  
+Se avete nomi da suggerire per possibili interviste, scrivetemi!  
+Sapete, la strategia delle interviste è un buon metodo per crescere nei follower!!! HEHEHE  
+A parte gli scherzi, potrebbe essere carino, no?
+
+Non mi resta che ringraziarvi per avermi seguito in questo primo mese, cercherò di migliorare ancora nell'audio e nella scrittura. Scriverò un po' di più dei luoghi e cercherò di approfondire alcuni argomenti, ma sappiate che spesso mi siedo qui senza nessuna idea, ma succede una sorta di magia, si apre la mente, arrivano i primi pensieri e poi come in un flow si incatena tutto il resto.  
+Questa è la vera terapia.
+
+Hai ascoltato **_InvisibilePerso_**, il diario in parole di un cammino tra emozioni e trasformazioni. Se ti va di approfondire, trovi tutto sul sito in descrizione. Per domande, pensieri o semplici saluti, scrivimi a **[invisibileperso@posteo.com](mailto:invisibileperso@posteo.com)**. Aiutami a far conoscere questo progetto: condividilo con chi potrebbe apprezzarlo. Grazie per essere stato con me, ci sentiamo domani!`
     }
     // Puoi aggiungere altre entries di febbraio qui
 }; 
