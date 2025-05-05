e8fe8546db5f546537e2a07850d803bed9cff791
invisibileperso <invisibileperso@posteo.com>
2025-05-05 12:51:56 +0200

    Create entries-may.js

---
+++ entries-may.js
@@ -0,0 +1,18 @@
+const entriesMay = {
+    "86": {
+        spotify: `<iframe style="border-radius:12px" 
+            src="https://open.spotify.com/embed/episode/6PX4NzYRC8LNlj34XJC32m?utm_source=generator" 
+            width="100%" 
+            height="152" 
+            frameBorder="0" 
+            allowfullscreen="" 
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+            loading="lazy">
+        </iframe>`,
+        content: ` Primo aprile duemilaventicinque
+
+Registrato direttamente con il telefono
+Pesce d'aprile
+#86  `
+    },
+}; 
