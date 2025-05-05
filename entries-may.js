793f255ecbbb1aa939bd5f5d28cf2c2589d7e33d
invisibileperso <invisibileperso@posteo.com>
2025-05-05 13:08:15 +0200

    Update entries-may.js

--- entries-may.js
+++ entries-may.js
@@ -13,6 +13,70 @@ const entriesMay = {
 
 Registrato direttamente con il telefono
 Pesce d'aprile
-#86  `
+#86  
+Ciao,  
+come va?
+
+==Direi che andava meglio ieri==  
+Eh sì, vecchio mio, concordo.
+
+Lo sai che siamo arrivati a maggio?  
+L'ultima volta che ci eravamo sentiti era l'ultimo giorno di aprile e oggi siamo già al 5 maggio.  
+Il periodo di stop più lungo dall'inizio di questo InvisibilePerso.
+
+Non avevo programmato nessuno stop e nessuna puntata, mi ero detto: "vedi come va e se riesci pubblica, altrimenti goditi il silenzio".  
+E così è andata.  
+Vi ho pensato, ogni tanto pensavo che quella cosa che mi stava succedendo sarebbe stata ottima per InvisibilePerso, ma insomma, eccomi qua con zero appunti, una montagna di stanchezza fisica e zero voglia di ripartire.
+
+Sono state giornate di lavoro casalingo: ho finito l’orto 4.0, fatto qualche lavoretto a casa dei nonni e cercato — senza successo — un albero per il giardino. E, udite udite, sono riuscito anche a fare due giri in bici…"
+Eee sì, miei cari, sono riuscito a fare due girelli con la bici: non mi capitava da mesi, forse anni?  
+Boh... tantissimo tempo comunque, ma ci tornerò tra un po'.
+
+In questi giorni ho rafforzato quel pensiero del lavoro e della gabbia sociale che impedisce i travasi verso classi sociali superiori, che a dirla così fa proprio brutto, no?  
+Comunque sì, ho lavorato tanto, non ho toccato il PC, ho guardato poco o niente il telefono e mi è tornata una gran voglia di birra, di quelle beverine, poco alcoliche, fresche, dissetanti.
+
+Mi sono nuovamente scartavetrato le mani, tagliate con cacciaviti e altri arnesi, mi sono distrutto le spalle a forza di stringere quei cacchio di tubi e tubetti per fare l'irrigazione a goccia nell'orto, e ovviamente ho guardato lo snooker.  
+Rapido aggiornamento: si sta giocando la finale tra Williams e il cinese Xintong, non vi dico il punteggio per non spoilerarvi l'andamento, so che ci tenete.
+
+Oggi pomeriggio, dopo aver invano desiderato fare una pennica sul divano, mi sono messo a far la pizza e mentre ero lì in piedi al banco della cucina intento a tagliare e preparare gli ingredienti, una parte della mia mente era già ad oggi e scatenava in me una repulsione totale.  
+Avete presente la depressione di fine ferie, la sindrome della domenica sera?  
+Eccola là, perfetta, mi ha assalito con tutta la sua forza.  
+Un senso di vomito totale, una spasmodica ricerca di una scusa attendibile per non andare al lavoro, quasi un tornare adolescente per nascondersi dietro al nulla.  
+Che stati, che stato insopportabile.  
+Che schifo!
+
+Ma torniamo ai due giri in bici.  
+Pochi chilometri, ma come vi avevo già detto non mi interessano le lunghe distanze, ora voglio solo tornar a pedalare e pedalare forte, non lungo, ma forte!  
+Per ora non posso permettermi il lungo piano, tanto meno il forte, ma insomma, due uscite in tre giorni è tanta roba.  
+Tanta roba soprattutto perché sono a corto di vestiario... dopo che nel trasloco, il sacco con tutto il mio ben di Dio di vestiario è stato dato per sbaglio alla Caritas...
+
+Il primo giro, verso il mare: campagna totale, argini infiniti, vento di trequarti a favore e poi inesorabilmente contrario.  
+Niente computerino, niente cuffiette, solo un timer di 45 minuti per indicarmi il punto di svolta.  
+Solo un ordine: pedala fino a quando non suona il timer, poi voltati e pedala fino a casa.  
+Solo sensazioni, niente numeri, solo un occhio al pignone posteriore per vedere il rapporto che avevo in catena.  
+Mal di gambe, pochissimi watt, perché il muscolo quadricipite, banalmente quello che spinge, fa presto a perdere tono, e infatti a guardarmi le gambette, mi chiedevo dove fosse finito!
+
+La seconda uscita, giusto per dirvi da quanto non facevo una doppietta, è iniziata con il mal di culo...  
+Eee sì, miei cari, la bici ha i suoi lati negativi, ma calma e sangue freddo.  
+Ho fatto una risata per la situazione, io con il mal di culo, come i novellini.  
+Ok, vai tranquillo, qualche pedalata e tutto passa.  
+Ancora no computerino, strada dei colli, tranquillità totale finché non sento arrivare un trattore: mi si avvicina, vedo la prima ruota, è il classico trattorino che in questo periodo spruzza chimica nei vigneti.  
+Mi passa, accelero e faccio un mitico dietro-trattore.  
+Penso di essere sopra i 40 all'ora, ma ci sto comodo, finché non inizia uno strappo che il trattore, non curante, supera con leggerezza.  
+Io invece lo affronto con tutta la mia pesantezza!
+
+Ma bene così.  
+Concludo il giro ovviamente controvento, con le gambe che spingevano, per quel che possono, ma spingevano.
+
+In tutto questo ho meditato poco, ho fatto il secondo incontro di respirazione nella meditazione, e anche per la meditazione, forse, la fa chi se la può permettere.  
+La fatica fisica straboccante inondava anche i campi dell'energia mentale, allagando quelle rare pozze meditative.
+
+Fatica, miei cari, e nulla in apparenza è cambiato, o sono io che vorrei vedere solo un cambiamento?  
+Forse dovrei meravigliarmi di più per tutto quello che cambia attorno?  
+Non lo so, ma almeno ho ritrovato la bici.
+
+A domani.
+
+`
     },
 }; 
