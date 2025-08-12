c313b773f8145cc534da1a0221684c1ec6b39b6c
invisibileperso <invisibileperso@posteo.com>
2025-08-12 14:58:37 +0200

    Update entries-august.js

--- entries-august.js
+++ entries-august.js
@@ -1,86 +1,86 @@
 const entriesAugust = {
     "150": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/150.md'
+        file: 'Agosto25/150.md'
     },
     "151": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/2" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/151.md'
+        file: 'Agosto25/151.md'
     },
     "152": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/3" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/152.md'
+        file: 'Agosto25/152.md'
     },
     "153": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/4" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/153.md'
+        file: 'Agosto25/153.md'
     },
     "154": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/5" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/154.md'
+        file: 'Agosto25/154.md'
     },
     "155": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/6" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/155.md'
+        file: 'Agosto25/155.md'
     },
     "156": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/7" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/156.md'
+        file: 'Agosto25/156.md'
     },
     "157": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/8" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/157.md'
+        file: 'Agosto25/157.md'
     },
     "158": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/9" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/158.md'
+        file: 'Agosto25/158.md'
     },
     "159": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/159.md'
+        file: 'Agosto25/159.md'
     },
     "160": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator1" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/160.md'
+        file: 'Agosto25/160.md'
     },
     "161": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator2" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/161.md'
+        file: 'Agosto25/161.md'
     },
     "162": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator3" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/162.md'
+        file: 'Agosto25/162.md'
     },
     "163": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator4" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/163.md'
+        file: 'Agosto25/163.md'
     },
     "164": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator5" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/164.md'
+        file: 'Agosto25/164.md'
     },
     "165": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator6" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/165.md'
+        file: 'Agosto25/165.md'
     },
     "166": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator7" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/166.md'
+        file: 'Agosto25/166.md'
     },
     "167": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator8" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/167.md'
+        file: 'Agosto25/167.md'
     },
     "168": {
         spotify: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/PUT_YOUR_SONG_ID_HERE?utm_source=generator9" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/168.md'
+        file: 'Agosto25/168.md'
     },
     "169": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/20" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/169.md'
+        file: 'Agosto25/169.md'
     },
     "170": {
         spotify: `<iframe style="border-radius:12px" src="http://googleusercontent.com/spotify.com/21" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-        file: 'agosto25/170.md'
+        file: 'Agosto25/170.md'
     }
 };
